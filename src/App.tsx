ae0daa38c00af01e17c00d2da11a97dd8886c3c9
Ben Rogers <ben@bdr.org>
2019-04-29 22:08:26 +0100

    Refactored withRoot inside App

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,17 @@ import { Route, Router } from "react-router";
 import { createBrowserHistory } from "history";
 import { Link, Switch } from "react-router-dom";
 import Trains from "./Components/Trains";
-import withRoot from "./withRoot";
-import {
-  AppBar,
-  Typography,
-  Toolbar,
-  Button,
-  createStyles,
-  withStyles,
-  WithStyles
-} from "@material-ui/core";
+import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
 import Blah from "./Components/Blah";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/styles";
+
+const theme = createMuiTheme({});
 
 const history = createBrowserHistory();
 
-const styles = createStyles({
+const useStyles = makeStyles({
   appBar: {
     marginBottom: 20
   },
@@ -28,27 +24,34 @@ const styles = createStyles({
   }
 });
 
-const App = ({ classes }: WithStyles<typeof styles>) => (
-  <Router history={history}>
-    <div className="App">
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            <Link to="/trains" className={classes.link}>
-              <Button className={classes.link}>Trains</Button>
-            </Link>
-            <Link to="/blah" className={classes.link}>
-              <Button className={classes.link}>Blah</Button>
-            </Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Switch>
-        <Route path="/trains" component={Trains} />
-        <Route path="/blah" component={Blah} />
-      </Switch>
-    </div>
-  </Router>
-);
+const App = () => {
+  const classes = useStyles();
 
-export default withRoot(withStyles(styles)(App));
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router history={history}>
+        <div className="App">
+          <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" color="inherit" noWrap>
+                <Link to="/trains" className={classes.link}>
+                  <Button className={classes.link}>Trains</Button>
+                </Link>
+                <Link to="/blah" className={classes.link}>
+                  <Button className={classes.link}>Blah</Button>
+                </Link>
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            <Route path="/trains" component={Trains} />
+            <Route path="/blah" component={Blah} />
+          </Switch>
+        </div>
+      </Router>
+    </MuiThemeProvider>
+  );
+};
+
+export default App;
